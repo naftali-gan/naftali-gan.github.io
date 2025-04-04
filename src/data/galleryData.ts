af44ce65d42530a8b53de1f9dacca560cf7ac2e7
Lidor Itzhari <lidor@gotech.io>
2025-04-04 15:02:26 +0300

    [WIP] All photos added

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -104,6 +104,186 @@ export const galleryItems: GalleryItem[] = [
     id: 20,
     image: "/images/gallery/fullsize/gallery20.webp",
     thumbnail: "/images/gallery/thumbnails/gallery20.webp"
+  },
+  {
+    id: 21,
+    image: "/images/gallery/fullsize/gallery21.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery21.webp"
+  },
+  {
+    id: 22,
+    image: "/images/gallery/fullsize/gallery22.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery22.webp"
+  },
+  {
+    id: 23,
+    image: "/images/gallery/fullsize/gallery23.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery23.webp"
+  },
+  {
+    id: 24,
+    image: "/images/gallery/fullsize/gallery24.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery24.webp"
+  },
+  {
+    id: 25,
+    image: "/images/gallery/fullsize/gallery25.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery25.webp"
+  },
+  {
+    id: 26,
+    image: "/images/gallery/fullsize/gallery26.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery26.webp"
+  },
+  {
+    id: 27,
+    image: "/images/gallery/fullsize/gallery27.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery27.webp"
+  },
+  {
+    id: 28,
+    image: "/images/gallery/fullsize/gallery28.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery28.webp"
+  },
+  {
+    id: 29,
+    image: "/images/gallery/fullsize/gallery29.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery29.webp"
+  },
+  {
+    id: 30,
+    image: "/images/gallery/fullsize/gallery30.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery30.webp"
+  },
+  {
+    id: 31,
+    image: "/images/gallery/fullsize/gallery31.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery31.webp"
+  },
+  {
+    id: 32,
+    image: "/images/gallery/fullsize/gallery32.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery32.webp"
+  },
+  {
+    id: 33,
+    image: "/images/gallery/fullsize/gallery33.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery33.webp"
+  },
+  {
+    id: 34,
+    image: "/images/gallery/fullsize/gallery34.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery34.webp"
+  },
+  {
+    id: 35,
+    image: "/images/gallery/fullsize/gallery35.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery35.webp"
+  },
+  {
+    id: 36,
+    image: "/images/gallery/fullsize/gallery36.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery36.webp"
+  },
+  {
+    id: 37,
+    image: "/images/gallery/fullsize/gallery37.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery37.webp"
+  },
+  {
+    id: 38,
+    image: "/images/gallery/fullsize/gallery38.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery38.webp"
+  },
+  {
+    id: 39,
+    image: "/images/gallery/fullsize/gallery39.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery39.webp"
+  },
+  {
+    id: 40,
+    image: "/images/gallery/fullsize/gallery40.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery40.webp"
+  },
+  {
+    id: 41,
+    image: "/images/gallery/fullsize/gallery41.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery41.webp"
+  },
+  {
+    id: 42,
+    image: "/images/gallery/fullsize/gallery42.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery42.webp"
+  },
+  {
+    id: 43,
+    image: "/images/gallery/fullsize/gallery43.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery43.webp"
+  },
+  {
+    id: 44,
+    image: "/images/gallery/fullsize/gallery44.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery44.webp"
+  },
+  {
+    id: 45,
+    image: "/images/gallery/fullsize/gallery45.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery45.webp"
+  },
+  {
+    id: 46,
+    image: "/images/gallery/fullsize/gallery46.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery46.webp"
+  },
+  {
+    id: 47,
+    image: "/images/gallery/fullsize/gallery47.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery47.webp"
+  },
+  {
+    id: 48,
+    image: "/images/gallery/fullsize/gallery48.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery48.webp"
+  },
+  {
+    id: 49,
+    image: "/images/gallery/fullsize/gallery49.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery49.webp"
+  },
+  {
+    id: 50,
+    image: "/images/gallery/fullsize/gallery50.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery50.webp"
+  },
+  {
+    id: 51,
+    image: "/images/gallery/fullsize/gallery51.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery51.webp"
+  },
+  {
+    id: 52,
+    image: "/images/gallery/fullsize/gallery52.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery52.webp"
+  },
+  {
+    id: 53,
+    image: "/images/gallery/fullsize/gallery53.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery53.webp"
+  },
+  {
+    id: 54,
+    image: "/images/gallery/fullsize/gallery54.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery54.webp"
+  },
+  {
+    id: 55,
+    image: "/images/gallery/fullsize/gallery55.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery55.webp"
+  },
+  {
+    id: 56,
+    image: "/images/gallery/fullsize/gallery56.webp",
+    thumbnail: "/images/gallery/thumbnails/gallery56.webp"
   }
 ];
 
